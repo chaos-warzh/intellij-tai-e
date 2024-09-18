44d0b9fdef870859b218f736aeec01c05bff6087
chaos-warzh <haidedeyu@163.com>
2024-09-19 01:52:30 +0800

    Fix ToolWin bugs

--- src/main/java/pascal/taie/intellij/core/ToolWindowFactoryImpl.java
+++ src/main/java/pascal/taie/intellij/core/ToolWindowFactoryImpl.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 import pascal.taie.intellij.gui.toolwindow.view.TaintToolWindowPanel;
@@ -13,7 +14,8 @@ public final class ToolWindowFactoryImpl implements ToolWindowFactory {
     public void createToolWindowContent(@NotNull final Project project, @NotNull final ToolWindow toolWindow) {
         // for taint analysis:
         final TaintToolWindowPanel taintToolWin = new TaintToolWindowPanel(project, toolWindow);
-        toolWindow.getContentManager().addContent(ApplicationManager.getApplication().getService(ContentFactory.class)
-                .createContent(taintToolWin.getRootPanel(), "", false));
+        final ContentFactory contentFactory = ApplicationManager.getApplication().getService(ContentFactory.class);
+        final Content content = contentFactory.createContent(taintToolWin.getRootPanel(), "", false);
+        toolWindow.getContentManager().addContent(content);
     }
 }
