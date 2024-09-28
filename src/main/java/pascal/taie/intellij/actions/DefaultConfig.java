2e58a2c8aad98e66e2868da5aa9ce5d769196816
chaos-warzh <haidedeyu@163.com>
2024-09-29 02:50:55 +0800

    +Default Actions

---
+++ src/main/java/pascal/taie/intellij/actions/DefaultConfig.java
@@ -0,0 +1,15 @@
+package pascal.taie.intellij.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+import pascal.taie.intellij.analysis.AnalysisStatus;
+
+public abstract class DefaultConfig extends AbstractAction {
+
+    @Override
+    protected boolean isEnabled(@NotNull AnActionEvent e, @NotNull Project project, @NotNull AnalysisStatus status) {
+        return !status.isRunning();
+    }
+
+}
