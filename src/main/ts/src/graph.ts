72d4d34016a97b9604ef9e50d40d2746c788eb69
BryanHeBY <bryanheby@qq.com>
2024-11-07 17:19:26 +0800

    add: adapt webview front-end for FlowGraph

--- src/main/ts/src/graph.ts
+++ src/main/ts/src/graph.ts
@@ -2,12 +2,24 @@ import { parse } from "yaml";
 
 export class Graph{
     metadata: Metadata;
-    relation: Relation;
+    parent: Map<number, number>;
+    children: Map<number, number[]>;
     graph: Map<number, number[]>;
 
     constructor(data: any){
         this.metadata = new Metadata(data.metadata);
-        this.relation = new Relation(data.relation, this.metadata);
+        this.parent = new Map();
+        Object.entries(data.parent).forEach(entry=>{
+            this.parent.set(parseInt(entry[0]), entry[1] as number);
+        });
+        this.children = new Map();
+        Object.entries(data.children).forEach(entry=>{
+            const children: number[] = [];
+            (entry[1] as number[]).forEach(n=>{
+                children.push(n);
+            });
+            this.children.set(parseInt(entry[0]), children);
+        });
         this.graph = new Map();
         this.initGraph(data.graph);
     }
@@ -16,159 +28,60 @@ export class Graph{
         Object.entries(graph).forEach(entry=>{
             const toNodess: number[] = [];
             (entry[1] as number[]).forEach(n=>{
-                if(this.isField(n)){
-                    toNodess.push(...(graph[n] as number[]));
-                }else{
-                    toNodess.push(n);
-                }
+                toNodess.push(n);
             });
             const toNodes = Array.from(new Set(toNodess));
             this.graph.set(parseInt(entry[0]), toNodes);
         });
     }
 
-    isField(num:number){
-        return this.relation.fields.includes(num);
-    }
-
-
-    isSourcePoint(index: number) {
-        return this.metadata.isSourcePoint(index);
-    }
-
-    isSinkPoint(index: number) {
-        return this.metadata.isSinkPoint(index);
-    }
-
-    isSource(index: number) {
-        return this.metadata.isSource(index);
-    }
-
-    isSink(index: number) {
-        return this.metadata.isSink(index);
+    getParent(node: number){
+        // if not exist, return -1
+        return this.parent.get(node) || -1;
     }
 }
 
 class Metadata{
-    packages: string[];
-    classes: string[];
-    methods: string[];
-    nodes: string[];
-    sourcePoints: number[];
-    sinkPoints: number[];
-    sources: number[];
-    sinks: number[];
+    vars: Node[];
+    groups: Map<string, number[]>;
 
     constructor(metadata: any){
-        this.packages = metadata.packages;
-        this.classes = metadata.classes;
-        this.methods = metadata.methods;
-        this.nodes = metadata.nodes;
-        this.sourcePoints = metadata.sourcePoints;
-        this.sinkPoints = metadata.sinkPoints;
-        this.sources = metadata.source;
-        this.sinks = metadata.sink;
-
-        const len: number = this.classes.length;
-        for(let i = 0; i < len; i++){
-            if(!this.classes[i].includes('.')){
-                // set the class with no parent package different to its package
-                this.classes[i] = '.' + this.classes[i];
-            }
-        }
+        this.vars = [];
+        metadata.vars.forEach((node: any)=>{
+            this.vars.push(new Node(node));
+        });
+        this.groups = new Map();
+        Object.entries(metadata.groups).forEach(entry=>{
+            const vars: number[] = [];
+            (entry[1] as number[]).forEach(n=>{
+                vars.push(n);
+            });
+            this.groups.set(entry[0], vars);
+        });
     }
 
-    getp(index: number) {
-        return this.packages[index];
-    }
-
-    getc(index: number) {
-        return this.classes[index];
-    }
-
-    getm(index: number) {
-        return this.methods[index];
-    }
-
-    getn(index: number) {
-        return this.nodes[index];
-    }
-
-    isSourcePoint(index: number) {
-        return this.sourcePoints.includes(index);
-    }
-
-    isSinkPoint(index: number) {
-        return this.sinkPoints.includes(index);
-    }
-
-    isSource(index: number) {
-        return this.sources.includes(index);
-    }
-
-    isSink(index: number) {
-        return this.sinks.includes(index);
+    get(index: number) {
+        return this.vars[index];
     }
 }
 
-class Relation{
-    packageToClasses: Map<number, number[]>;
-    classToMethods: Map<number, number[]>;
-    methodToNodes: Map<number, number[]>;
+class Node{
+    name: string;
+    isGroup: boolean;
+    group: string;
+    nodeType: string;
+    color: string;
 
-    fields: number[];
-    // jdkVars: number[];
-
-    constructor(relation: any, metadata: Metadata){
-        this.packageToClasses = new Map();
-        this.classToMethods = new Map();
-        this.methodToNodes = new Map();
-        this.fields = Object.values(relation.classToFields).flat() as number[];
-
-        // const jdkPackages: number[] = [];
-        Object.entries(relation.packageToClasses).forEach(entry=>{
-            this.packageToClasses.set(parseInt(entry[0]), entry[1] as number[]);
-            // if(metadata.getp(parseInt(entry[0])) === "java.lang"){ // todo:
-            //     jdkPackages.push(parseInt(entry[0]));
-            // }
-        });
-
-        Object.entries(relation.classToMethods).forEach(entry=>{
-            this.classToMethods.set(parseInt(entry[0]), entry[1] as number[]);
-        });
-
-        Object.entries(relation.methodToNodes).forEach(entry=>{
-            this.methodToNodes.set(parseInt(entry[0]), entry[1] as number[]);
-        });
-
-        // const jdkClasses: number[] = (Array.from(jdkPackages, p => this.packageToClasses.get(p)).flat().filter(item => item !== undefined) as number[]);
-        // const jdkMethods: number[] = (Array.from(jdkClasses, p => this.classToMethods.get(p)).flat().filter(item => item !== undefined) as number[]);
-        // this.jdkVars = (Array.from(jdkMethods, p => this.methodToVars.get(p)).flat().filter(item => item !== undefined) as number[]);
+    constructor(node: any){
+        this.name = node.name;
+        this.isGroup = node.isGroup;
+        this.group = node.group;
+        this.nodeType = node.nodeType;
+        this.color = node.color;
     }
-
 }
 
 export function build(yaml:string, visualize:(graph:Graph)=>void){
-    // const reader = new FileReader();
-    
-    // reader.onload = function(e){
-    //     const yamlContent = e.target?.result;
-    //     if(typeof yamlContent != 'string'){
-    //         console.log('yamlContent不是字符串类型');
-    //         return;
-    //     }
-    //     // try{
-    //         const parsedData = parse((yamlContent as string));
-    //         const graph:Graph = new Graph(parsedData);
-    //         console.log('finish build, begin visualization');
-    //         visualize(graph);
-    //     // }
-    //     // catch{
-    //     //     console.log(e);
-    //     // }
-    // }
-    
-    // reader.readAsText(file);
     const parsedData = parse(yaml);
     const graph:Graph = new Graph(parsedData);
     console.log('finish build, begin visualization');
--- src/main/ts/src/mockdata.ts
+++ src/main/ts/src/mockdata.ts
@@ -1,58 +1,145 @@
 import Mock from "mockjs";
 
-export const mockYaml = Mock.mock(`
+export const mockYaml = Mock.mock(
+`
 metadata:
-  packages:
-    - "pascal.taie.TEST"
-  classes:
-    - "pascal.taie.TEST.Main"
-    - "pascal.taie.TEST.SourceSink"
-  methods:
-    - "<pascal.taie.TEST.Main: void main(java.lang.String[])>"
-    - "<pascal.taie.TEST.SourceSink: java.lang.String source()>"
-    - "<pascal.taie.TEST.SourceSink: void sink(java.lang.String)>"
-  nodes:
-    - "VarNode{<pascal.taie.TEST.Main: void main(java.lang.String[])>/$r0}"
-    - "<pascal.taie.TEST.Main: void main(java.lang.String[])>[0@L5] $r0 = invokestatic pascal.taie.TEST.SourceSink.source()/result"
-    - "<pascal.taie.TEST.Main: void main(java.lang.String[])>[1@L6] invokestatic pascal.taie.TEST.SourceSink.sink($r0)/0"
-    - "{ kind: 'call', method: '<pascal.taie.TEST.SourceSink: java.lang.String source()>', index: 'result', type: 'java.lang.String' }"
-    - "{ method: '<pascal.taie.TEST.SourceSink: void sink(java.lang.String)>', index: '0' }"
-  sourcePoints:
-    - 1
-  sinkPoints:
-    - 2
-  source:
-    - 3
-  sink:
-    - 4
-relation:
-  packageToClasses:
-    0:
+  vars:
+    - name: "InstanceFieldNode{NewObj{<pascal.taie.TEST.Main: void main(java.lang.String[])>[1@L6] new pascal.taie.TEST.TMP}.str}"
+      isGroup: false
+      groupType: "node"
+      nodeType: "node"
+      color: ""
+    - name: "VarNode{<pascal.taie.TEST.Main: void main(java.lang.String[])>/$r0}"
+      isGroup: false
+      groupType: "node"
+      nodeType: "node"
+      color: ""
+    - name: "VarNode{<pascal.taie.TEST.Main: void main(java.lang.String[])>/$r2}"
+      isGroup: false
+      groupType: "node"
+      nodeType: "node"
+      color: ""
+    - name: "<pascal.taie.TEST.Main: void main(java.lang.String[])>[0@L5] $r0 = invokestatic pascal.taie.TEST.SourceSink.source()/result"
+      isGroup: false
+      groupType: "node"
+      nodeType: "source-point"
+      color: "lightcoral"
+    - name: "<pascal.taie.TEST.Main: void main(java.lang.String[])>[5@L8] invokestatic pascal.taie.TEST.SourceSink.sink($r2)/0"
+      isGroup: false
+      groupType: "node"
+      nodeType: "sink-point"
+      color: "lightgreen"
+    - name: "{ kind: 'call', method: '<pascal.taie.TEST.SourceSink: java.lang.String source()>', index: 'result', type: 'java.lang.String' }"
+      isGroup: false
+      groupType: "node"
+      nodeType: "source"
+      color: "gold"
+    - name: "{ method: '<pascal.taie.TEST.SourceSink: void sink(java.lang.String)>', index: '0' }"
+      isGroup: false
+      groupType: "node"
+      nodeType: "sink"
+      color: "aquamarine"
+    - name: "<pascal.taie.TEST.Main: void main(java.lang.String[])>"
+      isGroup: true
+      groupType: "method"
+      nodeType: ""
+      color: ""
+    - name: "<pascal.taie.TEST.SourceSink: java.lang.String source()>"
+      isGroup: true
+      groupType: "method"
+      nodeType: ""
+      color: ""
+    - name: "<pascal.taie.TEST.SourceSink: void sink(java.lang.String)>"
+      isGroup: true
+      groupType: "method"
+      nodeType: ""
+      color: ""
+    - name: "pascal.taie.TEST.Main"
+      isGroup: true
+      groupType: "class"
+      nodeType: ""
+      color: ""
+    - name: "pascal.taie.TEST.SourceSink"
+      isGroup: true
+      groupType: "class"
+      nodeType: ""
+      color: ""
+    - name: "pascal.taie.TEST.TMP"
+      isGroup: true
+      groupType: "class"
+      nodeType: ""
+      color: ""
+    - name: "pascal.taie.TEST"
+      isGroup: true
+      groupType: "package"
+      nodeType: ""
+      color: ""
+  groups:
+    node:
       - 0
-      - 1
-  classToMethods:
-    0:
-      - 0
-    1:
       - 1
       - 2
-  classToFields: {}
-  methodToNodes:
-    0:
-      - 0
-      - 1
-      - 2
-    1:
       - 3
-    2:
       - 4
-graph:
-  1:
+      - 5
+      - 6
+    package:
+      - 13
+    method:
+      - 7
+      - 8
+      - 9
+    class:
+      - 10
+      - 11
+      - 12
+parent:
+  0: 12
+  1: 7
+  2: 7
+  3: 7
+  4: 7
+  5: 8
+  6: 9
+  7: 10
+  8: 11
+  9: 11
+  10: 13
+  11: 13
+  12: 13
+children:
+  7:
+    - 1
+    - 2
+    - 3
+    - 4
+  8:
+    - 5
+  9:
+    - 6
+  10:
+    - 7
+  11:
+    - 8
+    - 9
+  12:
     - 0
+  13:
+    - 10
+    - 11
+    - 12
+graph:
   0:
     - 2
-  3:
-    - 1
+  1:
+    - 0
   2:
     - 4
-`);
+  3:
+    - 1
+  4:
+    - 6
+  5:
+    - 3
+`
+);
